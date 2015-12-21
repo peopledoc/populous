546f2ba7bed8b90ff05a168b956d552bba1d57f4
Yannick PEROUX <yannick.peroux@gmail.com>
2015-12-21 16:56:21 +0100

    Add hello world

---
+++ populous/__init__.py
@@ -0,0 +1,9 @@
+"""
+Populate your database with god-like powers
+"""
+
+__author__ = "People Doc"
+__copyright__ = "Copyright 2015, People Doc"
+__license__ = "MIT"
+__url__ = 'http://github.com/novafloss/populous'
+__version__ = "0.1a"
---
+++ populous/__main__.py
@@ -0,0 +1,10 @@
+import click
+
+
+@click.command()
+def populous():
+    click.echo("Hello World!")
+
+
+if __name__ == '__main__':
+    populous()
