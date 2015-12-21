aa1b1a5f2742215e51bd64a5d2bf02b62e8414fd
Yannick PEROUX <yannick.peroux@gmail.com>
2015-12-21 16:56:29 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+import populous
+
+requirements = [
+    "click",
+]
+
+setup(
+    name="populous",
+    version=populous.__version__,
+    url=populous.__url__,
+    description=populous.__doc__,
+    author=populous.__author__,
+    license=populous.__license__,
+    long_description="TODO",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'populous = populous.__main__:populous'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Utilities",
+    ],
+    keywords='populous populate database',
+)
