487502d34573459d540119481f53b2f08d539b0a
Yannick PEROUX <yannick.peroux@gmail.com>
2015-12-22 18:52:14 +0100

    Allow blank World description

--- demo/demo/models.py
+++ demo/demo/models.py
@@ -16,7 +16,7 @@ GENDERS = (
 
 class World(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(blank=True, default='')
 
 
 class City(models.Model):
