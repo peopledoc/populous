f306f78304d657a163b2a03284c83afc09271e2b
Yannick PEROUX <yannick.peroux@gmail.com>
2015-12-29 13:23:41 +0100

    Add a naive implementation of the predict command

--- populous/cli.py
+++ populous/cli.py
@@ -1,7 +1,25 @@
 import click
 
+from .loader import load_yaml
+from .blueprint import Blueprint
+
 
 @click.group()
 @click.version_option()
 def cli():
     pass
+
+
+@cli.command()
+@click.argument('files', nargs=-1)
+def predict(files):
+    """
+    Predict how many objects will be created if the given files are used.
+    """
+    blueprint = Blueprint.from_description(load_yaml(*files))
+
+    for name, item in blueprint.items.items():
+        click.echo("{name}: {count} {by}".format(
+            name=name, count=item.count.number,
+            by="by {}".format(item.count.by) if item.count.by else ""
+        ))
