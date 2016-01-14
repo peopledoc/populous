58a1a0c39841ccaa530188f6edb675b3b90d6040
Yannick PEROUX <yannick.peroux@gmail.com>
2016-01-14 17:08:16 +0100

    AutoIncrement: Start at 1, like most dbs

--- populous/generators/autoincrement.py
+++ populous/generators/autoincrement.py
@@ -13,18 +13,11 @@ class AutoIncrement(Generator):
     @property
     def start(self):
         if self._start is None:
-            self._start = self._get_start()
+            backend = self.blueprint.backend
+            value = backend.get_max_existing_value(self.item, self.field_name)
+            self._start = value + 1 if value is not None else 1
 
         return self._start
-
-    def _get_start(self):
-        backend = self.blueprint.backend
-
-        if not backend:
-            return 0
-
-        value = backend.get_max_existing_value(self.item, self.field_name)
-        return value + 1 if value is not None else 0
 
     def generate(self):
         for i in count(self.start):
