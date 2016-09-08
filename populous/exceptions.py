e17f496d94179449a39d08ba04649224e442138e
Yannick PEROUX <yannick.peroux@gmail.com>
2016-09-08 11:20:59 +0200

    Display filename in validation errors when possible

--- populous/exceptions.py
+++ populous/exceptions.py
@@ -11,7 +11,18 @@ class YAMLError(PopulousError):
 
 
 class ValidationError(PopulousError):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super(ValidationError, self).__init__(*args, **kwargs)
+        self.filename = kwargs.pop('filename', None)
+
+    @property
+    def message(self):
+        msg = super(ValidationError, self).message
+        if self.filename:
+            return "File '{}': {}".format(self.filename, msg)
+        else:
+            return msg
 
 
 class BackendError(PopulousError):
