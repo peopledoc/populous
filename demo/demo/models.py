0cfce1a416b00e94aebc1d02da2479d1b5d8ef50
Yannick PEROUX <yannick.peroux@gmail.com>
2016-09-14 13:05:09 +0200

    Demo: Add uniqueness to world name

--- demo/demo/models.py
+++ demo/demo/models.py
@@ -15,7 +15,7 @@ GENDERS = (
 
 
 class World(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     description = models.TextField(blank=True, default='')
 
 
