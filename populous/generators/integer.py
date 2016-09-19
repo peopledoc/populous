f42795a3d554e90d4040e085e8572225100c02b0
Yannick PEROUX <yannick.peroux@gmail.com>
2016-09-19 18:06:53 +0200

    Add to_string to IntegegerGenerator

--- populous/generators/integer.py
+++ populous/generators/integer.py
@@ -5,12 +5,19 @@ from .base import Generator
 
 class Integer(Generator):
 
-    def get_arguments(self, min=0, max=(2 ** 32) - 1, **kwargs):
+    def get_arguments(self, min=0, max=(2 ** 32) - 1, to_string=False,
+                      **kwargs):
         super(Integer, self).get_arguments(**kwargs)
 
         self.min = min
         self.max = max
+        self.to_string = to_string
 
     def generate(self):
+        if self.to_string:
+            return (str(e) for e in self._generate())
+        return self._generate()
+
+    def _generate(self):
         while True:
             yield random.randint(self.min, self.max)
