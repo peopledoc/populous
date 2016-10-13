04772c97308c8c0bed513913e12db8d562e8fa7c
Yannick PEROUX <yannick.peroux@gmail.com>
2016-10-13 17:26:27 +0200

    Add populous to the demo apps

--- demo/settings.py
+++ demo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'demo'
+    'demo.demo',
+    'populous.django.PopulousConfig'
 )
 
 MIDDLEWARE_CLASSES = (
