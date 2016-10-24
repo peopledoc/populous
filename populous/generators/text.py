137ebdff15c3072d20e5d20a6634393a07437c1a
Yannick PEROUX <yannick.peroux@gmail.com>
2016-10-24 18:59:15 +0200

    Text generator: Possibility to restrict the chars

--- populous/generators/text.py
+++ populous/generators/text.py
@@ -7,22 +7,39 @@ from .base import Generator
 
 class Text(Generator):
 
-    def get_arguments(self, min_length=0, max_length=None, **kwargs):
+    def get_arguments(self, min_length=0, max_length=None, chars='<a-Z><0-9> ',
+                      **kwargs):
         super(Text, self).get_arguments(**kwargs)
 
         self.min_length = min_length or 0
         self.max_length = max_length or 10000
+        self.chars = self.get_chars(chars)
 
-    def letters(self):
-        while True:
-            yield random.choice(string.printable)
+    def get_chars(self, description):
+        categories = {
+            '<a-Z>': string.ascii_letters,
+            '<a-z>': string.ascii_lowercase,
+            '<A-Z>': string.ascii_uppercase,
+            '<0-9>': string.digits,
+            '<spaces>': ' \t',
+            '<printable>': string.printable,
+            '<punctuation>': string.punctuation,
+            '<newline>': '\n',
+        }
+        for category, chars in categories.items():
+            description = description.replace(category, chars)
+
+        return description
 
     def generate(self):
-        letters = self.letters()
+        def _chars():
+            while True:
+                yield random.choice(self.chars)
+        chars = _chars()
 
         while True:
             yield ''.join(
                 itertools.islice(
-                    letters, random.randint(self.min_length, self.max_length)
+                    chars, random.randint(self.min_length, self.max_length)
                 )
             )
