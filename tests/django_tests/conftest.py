d63b661008b760565c5d625169ddb1d782ab05c1
Sébastien Eustace <sebastien.eustace@people-doc.com>
2017-10-12 15:59:09 +0200

    Remove unnecessary print

--- tests/django_tests/conftest.py
+++ tests/django_tests/conftest.py
@@ -7,5 +7,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 @pytest.fixture(autouse=True, scope='module')
 def setup_django():
     django = pytest.importorskip('django')
-    print(django.__dict__)
     django.setup()
