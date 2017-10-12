c173fce3cfb4a00183a8055c7a78dec37095c4c5
Sébastien Eustace <sebastien.eustace@people-doc.com>
2017-10-12 15:53:13 +0200

    Rename django tests folder to django_tests

--- tests/django_tests/conftest.py
+++ tests/django_tests/conftest.py
@@ -4,7 +4,8 @@ import pytest
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope='module')
 def setup_django():
     django = pytest.importorskip('django')
+    print(django.__dict__)
     django.setup()
