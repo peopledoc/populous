d99507ceeecbf278b48fd66499045baf026298a1
Yannick PÉROUX <yannick.peroux@gmail.com>
2017-11-27 11:19:00 +0100

    novafloss -> peopledoc

--- populous/__init__.py
+++ populous/__init__.py
@@ -6,5 +6,5 @@ __author__ = "PeopleDoc"
 __author_email__ = "yannick.peroux@people-doc.com"
 __copyright__ = "Copyright 2017, PeopleDoc"
 __license__ = "MIT"
-__url__ = 'http://github.com/novafloss/populous'
+__url__ = 'http://github.com/peopledoc/populous'
 __version__ = '0.3.0.dev0'
